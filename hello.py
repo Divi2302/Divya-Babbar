96327e407d95ed76d1c8bfea5db83e28d706b096
Divi2302 <thakur.sahab229090@gmail.com>
2026-01-22 14:14:46 +0530

    message

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+class EmailValidator:
+    def __init__(self,email):
+        self.email= email;
+
+    def validate(self):
+        if "@" not in self.email or "." not in self.email:
+            print("Not valid!")
+        else:
+            print("valid")
+        return
+
+    def get_domain(self):
+        print(self.email.split("@")[-1])
+    
+email= input("Enter your email to check: ")
+user_email= EmailValidator(email)
+user_email.validate()
+user_email.get_domain()
+    
